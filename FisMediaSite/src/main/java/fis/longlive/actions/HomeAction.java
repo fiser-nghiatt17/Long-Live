70d92efc0fcb3f07dbbd3923d7d9c3fd4eee4bb2
fiser-cuonglv13 <cuonglv13@fpt.com.vn>
2015-11-23 20:14:48 +0700

    added album url

--- FisMediaSite/src/main/java/fis/longlive/actions/HomeAction.java
+++ FisMediaSite/src/main/java/fis/longlive/actions/HomeAction.java
@@ -5,21 +5,13 @@ package fis.longlive.actions;
  */
 import fis.longlive.database.process.ProcessAlbum;
 import fis.longlive.database.table.Album;
-import fis.longlive.models.HomeDisplay;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HomeAction {
     private List<Album> album;
-    private List<HomeDisplay> homeDisplays = new ArrayList<>();
     public String execute() {
         album = ProcessAlbum.selectAllAlbum();
-        for (Album a : album) {
-            if (a.getPictures().size() > 0) {
-                homeDisplays.add(new HomeDisplay(a.getPictures().get(0).getPictureURL(), a.getUser().getFullname(), a.getAlbumName(), a.getLikeAmount(), a.getViewAmount()));
-            }
-        }
         return "success";
     }
 
@@ -29,14 +21,6 @@ public class HomeAction {
 
     public void setAlbum(List<Album> album) {
         this.album = album;
-    }
-
-    public List<HomeDisplay> getHomeDisplays() {
-        return homeDisplays;
-    }
-
-    public void setHomeDisplays(List<HomeDisplay> homeDisplays) {
-        this.homeDisplays = homeDisplays;
     }
 }
 
